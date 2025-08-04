8e1a6ca5aca56cf67381d8bac9e91b4fa9850824
MatMassu <matiasmassucco99@gmail.com>
2025-08-04 14:34:11 -0300

    Added simple navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,41 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div className="flex h-12 relative min-w-screen justify-center items-center bg-white">
+        <div className="text-black font-serif text-lg">Zapatos Massucco</div>
+        <div className="grid grid-cols-2 items-center">
+          <div className="absolute left-14">
+            <input
+              className="w- bg-transparent placeholder:text-slate-400 font-serif text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+              placeholder="Buscar..." 
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <button className="absolute left-3 font-serif text-slate-700 text-sm border border-slate-200 rounded-md pl-2 pr-2 py-2 transition duration-100 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow active:scale-95">
+            <img 
+              src="menu.svg" alt="menu" height="20" width="20"/>
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="grid grid-cols-2 items-center">
+          <button className="absolute right-3 font-serif text-slate-700 text-sm border border-slate-200 rounded-md pl-2 pr-2 py-2 transition duration-100 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow active:scale-95">
+            <img 
+              src="cart.svg" alt="menu" height="20" width="20"/>
+          </button>
+          <button className="absolute right-14 font-serif text-slate-700 text-sm border border-slate-200 rounded-md pl-2 pr-2 py-2 transition duration-100 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow active:scale-95">
+            <img 
+              src="account.svg" alt="menu" height="20" width="20"/>
+          </button>
+        </div>
+      </div>
   );
 }
+
+
+
+
+{/*
+  <button type="button" className="px-1 py-2 border-white-100 rounded-md text-lg font-semibold font-sans text-black bg-amber-600 hover:bg-amber-700 hover:text-white active:bg-amber-800
+  active:scale-95">
+    Continuar
+  </button>
+*/}
